41ac88d461e2ac5637e01d47568fa695214eb92c
John Godley <johng75@gmail.com>
2022-01-06 17:51:12 +0000

    Add a title and className to external links

--- external-link/index.js
+++ external-link/index.js
@@ -9,10 +9,12 @@ import React from 'react';
  *
  * @param {object} props - Component props
  * @param {string} props.url - URL
+ * @param {string|undefined} props.className - Classname
+ * @param {string|undefined} props.title - Title
  * @param {string|React|object} props.children - Child components
  */
-const ExternalLink = ( { url, children } ) => {
-	return <a href={ url } target="_blank" rel="noopener noreferrer">{ children }</a>;
+const ExternalLink = ( { url, children, title = undefined, className = undefined } ) => {
+	return <a href={ url } target="_blank" rel="noopener noreferrer" title={ title } className={ className }>{ children }</a>;
 };
 
 export default ExternalLink;
