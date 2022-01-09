17f773e1dcde9c97be961f44f9d12503325c6047
John Godley <johng75@gmail.com>
2022-01-09 07:51:44 +0000

    Add optional JSdoc params to externallink

--- external-link/index.js
+++ external-link/index.js
@@ -9,9 +9,9 @@ import React from 'react';
  *
  * @param {object} props - Component props
  * @param {string} props.url - URL
- * @param {string|undefined} props.className - Classname
- * @param {string|undefined} props.title - Title
- * @param {string|React|object} props.children - Child components
+ * @param {string|undefined} [props.className] - Classname
+ * @param {string|undefined} [props.title] - Title
+ * @param {string|React|object} [props.children] - Child components
  */
 const ExternalLink = ( { url, children, title = undefined, className = undefined } ) => {
 	return <a href={ url } target="_blank" rel="noopener noreferrer" title={ title } className={ className }>{ children }</a>;
