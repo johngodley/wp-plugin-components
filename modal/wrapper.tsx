a6fafa5ae8856e8cfc7157d498c1accaf5c475b6
John Godley <john@johngodley.com>
2026-01-17 14:41:25 +0000

    Pressing escape in a modal closes the modal

--- modal/wrapper.tsx
+++ modal/wrapper.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import clsx from 'clsx';
 import ModalContent from './content';
 import type { ModalProps } from './index';
@@ -7,14 +7,25 @@ const CLASS = 'wpl-modal_shown';
 
 type ModalWrapperProps = ModalProps;
 
-function ModalWrapper( { padding = true, ...rest }: ModalWrapperProps ) {
+function ModalWrapper( { padding = true, onClose, ...rest }: ModalWrapperProps ) {
+	const handleKeyDown = useCallback(
+		( event: KeyboardEvent ) => {
+			if ( event.key === 'Escape' ) {
+				onClose();
+			}
+		},
+		[ onClose ]
+	);
+
 	useEffect( () => {
 		document.body.classList.add( CLASS );
+		document.addEventListener( 'keydown', handleKeyDown );
 
 		return () => {
 			document.body.classList.remove( CLASS );
+			document.removeEventListener( 'keydown', handleKeyDown );
 		};
-	}, [] );
+	}, [ handleKeyDown ] );
 
 	const classes = clsx( {
 		'wpl-modal_wrapper': true,
@@ -26,7 +37,7 @@ function ModalWrapper( { padding = true, ...rest }: ModalWrapperProps ) {
 			<div className="wpl-modal_backdrop"></div>
 
 			<div className="wpl-modal_main">
-				<ModalContent { ...rest } />
+				<ModalContent onClose={ onClose } { ...rest } />
 			</div>
 		</div>
 	);
